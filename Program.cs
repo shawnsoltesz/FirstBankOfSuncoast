ffa04b238a9714a6a9b70ae37ac9717bb06a5e78
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-07 00:06:54 -0400

    added Transaction Flows to Data of PEDA

--- Program.cs
+++ Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 
+
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                //Console.WriteLine("This is not a valid entry. Action cancelled.");
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("   Welcome to First Bank of Suncoast  ");
+            Console.WriteLine("    * * * * * * * * * * * * * * * *   ");
+            Console.WriteLine("             Accounts Manager       \n");
+
+            Console.WriteLine("    *-*-*-*-*-*-*-MENU-*-*-*-*-*-*  \n");
+
+            Console.WriteLine("CHECKING ACCOUNT\n");
+
+            Console.WriteLine("(A)dd a new dinosaur to the collection");
+            Console.WriteLine("(R)emove a dinosaur from the collection");
+            Console.WriteLine("(T)ransfer a dinosaur's enclosure pen\n");
+
+            Console.WriteLine("SAVINGS ACCOUNT\n");
+
+            Console.WriteLine("(C)ollection details of all dinosaurs");
+            Console.WriteLine("(S)ummary of diet types");
+            Console.WriteLine("(V)iew dinosaurs and date acquired\n");
+            Console.WriteLine("(Q)uit application\n");
+
+            Console.WriteLine("Please input the number from the menu and press ENTER.\n");
+
+            var userChoice = Console.ReadLine();
+
+        }
+
+
+
     }
-  }
 }
+
